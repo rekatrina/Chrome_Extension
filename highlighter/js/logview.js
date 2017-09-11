30aebdffca336d86635eff68ac3652a977c9d665
rekatrina <re-katrina@163.com>
2017-09-11 17:16:58 +0800

    [A] 增加已解析标记、可处理日志筛选

--- highlighter/js/logview.js
+++ highlighter/js/logview.js
@@ -28,23 +28,31 @@ function parseEveryLine(trEl){
 }
 
 function parseLog(){
+	var log_selected = document.getElementsByClassName("combo_value")[0]
+	alert(log_selected.value)
+	if(log_selected.value.indexOf("adstainlog")!=-1){
+		alert("目前不支持此类型日志解析")
+		return
+	}
 	// locate code log
 	var log_win_content = document.getElementById("log_win_content")
 	var table = log_win_content.getElementsByClassName("code_table")[0].getElementsByTagName("tbody")[0]
 	var trList = table.getElementsByTagName("tr")
-
+	var flag_text = "parse by Log HighLight"
+	
 	// unsearch
 	if(trList[0].childElementCount == 1){ 
 		alert("请先提交查询")
 	}
 	else{
+		// 检测到已经解析过，不处理
+		if(trList[0].getElementsByTagName("td")[1].innerHTML==flag_text)
+			return
 		for(var i=1; i<trList.length; i++)
 		parseEveryLine(trList[i])
+		table.innerHTML = '<tr><td class="line_td" data-line-num="0"></td><td>' + flag_text + '</td></tr>' + table.innerHTML
 	}
-	var flag_row = document.createElement('<tr><td class="line_td" data-line-num="0">::before</td><td>done by Log HighLight</td></tr>')
-	
-	table.appendChild(flag_row)
-	alert("done")
+
 	//var firstLine = trList[1].getElementsByTagName("td")[1]
 }
 
@@ -57,13 +65,11 @@ function addButton(el){
 	el.appendChild(o)
 }
 
-// 1. add Button for log coloring
-//alert("find logview")
 var btn_tijiao = document.getElementById("search_btn")
 btn_tijiao.addEventListener('click', ClickSearch)
-//alert(document.getElementsByClassName("row ta_c").length)
-var btn_row = document.getElementsByClassName("row ta_c")[0]
-//alert(btn_row.childElementCount)
-addButton(btn_row)
 
-//alert("finish")
+var btn_row = document.getElementsByClassName("row ta_c")[0]
+
+
+
+addButton(btn_row)
